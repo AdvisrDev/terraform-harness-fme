db6effd4528161cd8e91b90fb45da911cf1a3359
root <cristiano.rosetti@gmail.com>
2025-06-19 22:51:19 +0000

    updated docs

--- use-cases/banking-platform-feature-flags/main.tf
+++ use-cases/banking-platform-feature-flags/main.tf
@@ -1,6 +1,6 @@
 # Feature Flags Module
 module "feature_flags" {
-  source = "app.harness.io/EeRjnXTnS4GrLG5VNNJZUw/terraform-harness-fme/split/"
+  source = "app.harness.io/EeRjnXTnS4GrLG5VNNJZUw/terraform-harness-fme/split"
 
   workspace_name    = var.workspace_name
   environment_name  = var.environment_name
